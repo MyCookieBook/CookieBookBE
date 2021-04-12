ccd4b87831ccf84c135ca3d432860e726e14e1e6
Zhibek <zhibek.abay@yahoo.de>
2021-04-13 00:00:16 +0200

    implemented add&edit recipe👨‍🍳

--- rest-service/src/main/java/de/cookiebook/restservice/RecipeController.java
+++ rest-service/src/main/java/de/cookiebook/restservice/RecipeController.java
@@ -1,5 +1,37 @@
 package de.cookiebook.restservice;
 
+import org.springframework.web.bind.annotation.*;
+@RestController
 public class RecipeController {
 
+	RecipeRepository recipeRepository; 
+	
+	public RecipeController(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+	}
+	
+    @RequestMapping(value = "/addrecipe", method = RequestMethod.POST)
+    public Recipe addRecipe(@RequestBody Recipe recipe) {
+        if(recipe.getTitle() == null ||
+                recipe.getIngredients() == null || recipe.getSteps() == null) { // was waren nochmal die Pflichtfelder??
+            return null;
+        }
+        recipeRepository.save(recipe);
+        System.out.println(recipe);
+        return recipe;
+    }
+    
+    @RequestMapping(value = "/edit-recipe", method = RequestMethod.POST)
+    public Recipe editRecipe(@RequestBody Recipe recipe) {
+        //check if there's no valid id, recipe name, ingredients or steps, then can't update
+        if(recipeRepository.findById(recipe.getId()) == null ||recipe.getTitle() == null ||
+                recipe.getIngredients() == null || recipe.getSteps() == null) {
+            System.out.println("Cannot find recipe.");
+            return null;
+        }
+        recipeRepository.save(recipe);
+        System.out.println(recipe);
+        return recipe;
+    }
 }
+	
