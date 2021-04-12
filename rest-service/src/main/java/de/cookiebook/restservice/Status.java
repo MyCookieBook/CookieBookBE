159fe0959ab527fb4b3ac58cc3e68d93b3e889b2
Zhibek <zhibek.abay@yahoo.de>
2021-04-12 22:25:23 +0200

    Initialization, Register and Login

---
+++ rest-service/src/main/java/de/cookiebook/restservice/Status.java
@@ -0,0 +1,7 @@
+package de.cookiebook.restservice;
+
+public enum Status {
+    SUCCESS,
+    USER_ALREADY_EXISTS,
+    FAILURE
+}
---
+++ rest-service/src/main/java/de/cookiebook/restservice/User.java
@@ -0,0 +1,67 @@
+package de.cookiebook.restservice;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Objects;
+@Entity
+@Table(name = "users")
+public class User {
+    private @Id @GeneratedValue long id;
+    private @NotBlank String email;
+    private @NotBlank String password;
+    private @NotBlank boolean loggedIn;
+    public User() {
+    }
+    public User(@NotBlank String email, 
+                @NotBlank String password) {
+        this.email = email;
+        this.password = password;
+        this.loggedIn = false;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+    public void setLoggedIn(boolean loggedIn) {
+        this.loggedIn = loggedIn;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password, 
+                            loggedIn);
+    }
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", loggedIn=" + loggedIn +
+                '}';
+    }
+}
