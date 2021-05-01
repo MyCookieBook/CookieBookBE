4f2eed68eae87e9d8878221b8aa6759b5ab01dd2
sinja742 <3pt5grfdYF4>
2021-05-01 23:17:15 +0200

    implementation authentication (test failed)

--- rest-service/src/main/java/de/cookiebook/restservice/config/AuthenticationConfigConstants.java
+++ rest-service/src/main/java/de/cookiebook/restservice/config/AuthenticationConfigConstants.java
@@ -1,9 +1,9 @@
-//package de.cookiebook.restservice.config;
-//
-//public class AuthenticationConfigConstants {
-//    public static final String SECRET = "Java_to_Dev_Secret";
-//    public static final long EXPIRATION_TIME = 10800000; // 3h = 10800000
-//    public static final String TOKEN_PREFIX = "Bearer ";
-//    public static final String HEADER_STRING = "Authorization";
-//    public static final String SIGN_UP_URL = "users/login";
-//}
+package de.cookiebook.restservice.config;
+
+public class AuthenticationConfigConstants {
+    public static final String SECRET = "Java_to_Dev_Secret";
+    public static final long EXPIRATION_TIME = 10800000; // 3h = 10800000
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final String SIGN_UP_URL = "users/login";
+}
---
+++ rest-service/src/main/java/de/cookiebook/restservice/service/AuthenticationUserDetailService.java
@@ -0,0 +1,24 @@
+package de.cookiebook.restservice.service;
+
+import de.cookiebook.restservice.user.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+public class AuthenticationUserDetailService implements UserDetailsService {
+    private final UserService userService;
+
+    public AuthenticationUserDetailService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.readUserByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException(email);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), Collections.emptyList());
+    }
+}
